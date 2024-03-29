c8747c27c7e547afe97750f593b06a64177eb8e5
alealien666 <andraale28@gamil.com>
2024-03-30 05:09:46 +0700

    add user Seeder

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -5,6 +5,7 @@ namespace Database\Factories;
 use Illuminate\Database\Eloquent\Factories\Factory;
 use Illuminate\Support\Str;
 
+
 /**
  * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\User>
  */
@@ -19,10 +20,9 @@ class UserFactory extends Factory
     {
         return [
             // 'name' => fake()->name(),
-            // 'email' => fake()->unique()->safeEmail(),
-            // 'role' => fake()->numberBetween(0, 1),
-            // 'email_verified_at' => now(),
-            // 'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
+            // 'email' => 'user123@gmail.com',
+            // 'role' => 2,
+            // 'password' => Hash::make('user123'), // password
             // 'remember_token' => Str::random(10),
         ];
     }
